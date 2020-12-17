0eaf5c3026931fa2b79c0784b88ce36f51c83727
Lamer-ON <lamer_on@mail.ru>
2020-12-17 05:26:29 +0300

    team modal all done

--- script.js
+++ script.js
@@ -24,17 +24,31 @@ document.addEventListener("DOMContentLoaded", function () {
     team_modal1 = document.querySelector(".our-team_popup-1"),
     team_modal3 = document.querySelector(".our-team_popup-3");
 
-
   //============ our-team
+
+  if (window.matchMedia("(min-width: 961px)").matches) {
+    team_card1.classList.add("click");
+    team_modal1.classList.add("active");
+    for (let i = 0; i < team_cardAll.length; i++) {
+      team_cardAll[i].addEventListener("click", () => {
+        for (let i = 0; i < team_modalAll.length; i++) {
+          team_modalAll[i].classList.remove("active");
+        }
+        for (let i = 0; i < team_cardAll.length; i++) {
+          team_cardAll[i].classList.remove("click");
+        }
+        team_modalAll[i].classList.add("active");
+        team_cardAll[i].classList.add("click");
+      });
+    }
+  }
 
   if (window.matchMedia("(max-width: 960px)").matches) {
     /* the viewport is at least 400 pixels wide */
-    console.log('960px');
     team_card1.classList.add("click");
     team_modal1.classList.add("active");
     team_card3.classList.add("click");
     team_modal3.classList.add("active");
-
 
     for (let i = 0; i < team_cardAll.length; i++) {
       team_cardAll[i].addEventListener("click", () => {
@@ -53,30 +67,15 @@ document.addEventListener("DOMContentLoaded", function () {
             team_cardAll[i].classList.remove("click");
           }
         }
-
-
-        //for (let i = 0; i < team_modalAll.length; i++) {
-        //  team_modalAll[i].classList.remove("active");
-        //}
-        //for (let i = 0; i < team_cardAll.length; i++) {
-        //  team_cardAll[i].classList.remove("click");
-        //}
-
         team_modalAll[i].classList.add("active");
         team_cardAll[i].classList.add("click");
-
       });
     }
-
-
-  } else {
-    /* the viewport is less than 400 pixels wide */
   }
 
-
-  if (window.matchMedia("(min-width: 1140px)").matches) {
-    team_card1.classList.add("click");
-    team_modal1.classList.add("active");
+  if (window.matchMedia("(max-width: 720px)").matches) {
+    team_card3.classList.remove("click");
+    team_modal3.classList.remove("active");
 
     for (let i = 0; i < team_cardAll.length; i++) {
       team_cardAll[i].addEventListener("click", () => {
@@ -85,21 +84,14 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         for (let i = 0; i < team_cardAll.length; i++) {
           team_cardAll[i].classList.remove("click");
+          team_cardAll[i].style.marginBottom = '50px';
         }
-
         team_modalAll[i].classList.add("active");
         team_cardAll[i].classList.add("click");
-
-
-
-        //cardAll[i].classList.add("active");
-        //ImgAll[i].classList.add("active");
-        //cardAll[i].classList.add("active");
+        team_cardAll[i].style.marginBottom = '650px';
       });
     }
-
   }
-
 
   //============ testimonials radio
   card1.classList.add("active");
